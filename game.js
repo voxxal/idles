250785de640ad06e5423b439ae3ced1adde6a595
v0Xal <58456527+v0Xal@users.noreply.github.com>
2019-12-02 22:17:59 -0800

    //NEW VAULTS!

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 //Currency
 var money = 0;
 var ore = 0;
-var vaultOverflow = 200;
+
 
 //Pickaxes
 var picks = [["Wood Pickaxe", 20 , 2],["Stone Pickaxe", 100 , 3],["Iron Pickaxe", 500 , 5], ["Steel Pickaxe", 1000 , 10], ["Infinity Pickaxe", 999999999999999999999999999999999999999999999999999999999999999999, 1000]]
@@ -13,7 +13,14 @@ var pickCost = 20;
 var mines = [["Coal Mine", 400 , 2],["Iron Mine", 2000 , 3],["Steel Mine", 10000 , 5], ["Gold Mine", 20000 , 10], ["Infinity Mine", 999999999999999999999999999999999999999999999999999999999999999999, 1000]]
 var sellPrice = 1;
 var nextMineNum = 0;
-var mineCost = 20;
+var mineCost = 400;
+
+//Vaults
+var vaults = [["Stone Vault", 100 , 400],["Iron Vault", 1000 , 800],["Steel Vault", 10000 , 1500], ["Gold Vault", 100000 , 5000], ["Infinity Mine", 999999999999999999999999999999999999999999999999999999999999999999, 1000]]
+var vaultOverflow = 200;
+var nextVaultNum = 0;
+var vaultCost = 100;
+
 
 //Mining Code
 function mine() {
@@ -70,5 +77,19 @@ function buyNextMine(){
     document.getElementById("ore").innerHTML = ore;
   document.getElementById("money").innerHTML = money;
     console.log(nextMineNum);
+  }
+}
+function buyNextVault(){
+  if (money >= vaults[nextVaultNum][1]){
+    vaultOverflow = vaults[nextVaultNum][2];
+    money -= vaultCost;
+    nextVaultNum++;
+    document.getElementById("vaultbuy").innerHTML = vaults[nextVaultNum][0] +	"&#32;" + "&#40;" + vaults[nextVaultNum][1] + "&#32;" + "Coins" + "&#41;";
+    document.getElementById("pickpower").innerHTML = pickPower;
+    document.getElementById("overflow").innerHTML = vaultOverflow;
+    document.getElementById("minepower").innerHTML = sellPrice;
+    document.getElementById("ore").innerHTML = ore;
+  document.getElementById("money").innerHTML = money;
+    console.log(nextVaultNum);
   }
 }
