d801752280cb9c8d5c58c5baaa5b8f851fd80f4d
v0Xal <aidenhohoho@gmail.com>
2020-03-20 14:29:29 -0700

    Deleted save.js

--- beta/js/game.js
+++ beta/js/game.js
@@ -251,3 +251,22 @@ function genEnergy(){
   game.energy+=1;
   updateView();
  }
+ function save(){
+   window.localStorage.clear();
+ var save = JSON.stringify(game);
+ window.localStorage.setItem("game", save);
+ console.log("Game Saved!");
+ $.notify("Game Saved", "success");
+ updateView();
+ }
+ setInterval(save,30000);
+ function load(){
+   game=JSON.parse(localStorage.getItem('game'));
+   updateView();
+ }
+ if(localStorage.getItem('game') !== null){
+ load();
+ $.notify("Game Loaded", "info");
+ }else{
+  $.notify("No Save Game Found", "warn");
+ }
