92415d66972721815b0f220c23d0a4426965e2d3
v0Xal <aidenhohoho@gmail.com>
2020-04-13 21:16:57 -0700

    made some changes

--- beta/js/game.js
+++ beta/js/game.js
@@ -1,272 +1,338 @@
 const standard = new ADNotations.StandardNotation();
 const scientific = new ADNotations.ScientificNotation();
 
-
 //                                _|      _|              _|
 //      _|      _|     _|_|       _|  _|       _|_|_|   _|
 //     _|  _|     _|    _|     _|  _|     _|    _|   _|
 //      _|         _|_|     _|      _|     _|_|_|   _|
 
 //Thank you for playing!
-//This is BETA version
+//this is a BETA version
 
 //Currency//
 var game = {
-"money" : 0,
- "ore" : 0,
-//Pickaxes//
- "picks" : [ [["Stick"],["Wood Pickaxe"],["Stone Pickaxe"], ["Iron Pickaxe"],["Steel Pickaxe"], ["Diamond Pickaxe"] ], ["Shock Pickaxe"] ],
- "pickPower" : 1,
- "pickLevel" : 1,
- "nextPickNum" : 0,
- "pickCost" : 10,
-//Mines//
-  "mines" : [["Copper Mine", 2000 , 7, 5],["Iron Mine", 100000 , 15, 10], ["Steel Mine", 2000000 , 30,15],["Diamond Mine", 3000000, 50, 20], ["Shock Mine", 1000000000, 100,25]],
-  "sellPrice" : 1,
-  "toughness" : 1,
-"nextMineNum" : 0,
- "mineCost" : 400,
-
-//Vaults//
-"vaults" : [["Stone Vault", 100 , 400],["Iron Vault", 1000 , 800],["Steel Vault", 10000 , 1500], ["Gold Vault", 100000 , 5000],["Diamond Vault", 1000000, 15000],["Shock Vault",1000000000 , 100000]],
-"vaultOverflow" : 200,
-"nextVaultNum" : 0,
-"vaultCost" : 100,
-
-//Miners//
-"miners" : 0,
-"minerPower" : 1,
-"minerCost" : 20,
-"ops" : 0,
-//Energy//
- "energy":0,
-//Upgrades//
-"area" : 0,
-"pickMultiply" : 1,
-"mineMultiply" : 1,
-"toughnessMultiply" : 1,
-"upgrades" : [
-  {
-    "name":"Power Pickaxe",
-    "discription":"Feel the Power! Pickaxe Power increased by 100%",
-    "cost":1500,
-    "buffType":"pickaxe",
-    "buffAmount":2,
-    "criteria":1000,
-    "created":false
+  money: 0,
+  ore: 0,
+  //Pickaxes//
+  pick:{
+    names:[
+      [
+        ["Stick"],
+        ["Wood Pickaxe"],
+        ["Stone Pickaxe"],
+        ["Iron Pickaxe"],
+        ["Steel Pickaxe"],
+        ["Diamond Pickaxe"],
+      ],
+      ["Shock Pickaxe"],
+    ],
+    level:1,
+    power:1,
+    cost:10,
+    multiply:1,
+    next:0
   },
-  {
-    "name":"Shiny Ores",
-    "discription":"The ore is now more shiny. Sell Price increased by 100%",
-    "cost":25000,
-    "buffType":"mine",
-    "buffAmount":2,
-    "criteria":20000,
-    "created":false
-  },
-  {
-    "name":"Stronger Picks",
-    "discription":"Give your Miners better Pickaxes. OPS increased by 100%",
-    "cost":15000,
-    "buffType":"miners",
-    "buffAmount":2,
-    "criteria":10000,
-    "created":false
-  }
-]
+
+  //Mines//
+  mines: [
+    [
+      ["Copper Mine", 2000, 7, 5],
+      ["Iron Mine", 100000, 15, 10],
+      ["Steel Mine", 2000000, 30, 15],
+      ["Diamond Mine", 3000000, 50, 20],
+    ],
+    [["Shock Mine", 1000000000, 100, 25]],
+  ],
+  sellPrice: 1,
+  toughness: 1,
+  nextMineNum: 0,
+  mineCost: 400,
+
+  //Vaults//
+  vaults: [
+    ["Stone Vault", 100, 400],
+    ["Iron Vault", 1000, 800],
+    ["Steel Vault", 10000, 1500],
+    ["Gold Vault", 100000, 5000],
+    ["Diamond Vault", 1000000, 15000],
+    ["Shock Vault", 1000000000, 100000],
+  ],
+  vaultOverflow: 200,
+  nextVaultNum: 0,
+  vaultCost: 100,
+
+  //Miners//
+  miners: 0,
+  minerPower: 1,
+  minerCost: 20,
+  ops: 0,
+
+  //Energy//
+  energy: 0,
+  allBoost: 1,
+  ticks: 1000,
+  //Upgrades//
+  area: 0,
+  mineMultiply: 1,
+  toughnessMultiply: 1,
+  upgrades: [
+    {
+      name: "Power Pickaxe",
+      discription: "Feel the Power! Pickaxe Power increased by 100%",
+      cost: 1500,
+      buffType: "pickaxe",
+      buffAmount: 2,
+      criteria: 1000,
+      created: false,
+    },
+    {
+      name: "Shiny Ores",
+      discription: "The ore is now more shiny. Sell Price increased by 100%",
+      cost: 25000,
+      buffType: "mine",
+      buffAmount: 2,
+      criteria: 20000,
+      created: false,
+    },
+    {
+      name: "Stronger Picks",
+      discription: "Give your Miners better Pickaxes. OPS increased by 100%",
+      cost: 15000,
+      buffType: "miners",
+      buffAmount: 2,
+      criteria: 10000,
+      created: false,
+    },
+  ],
 };
 //Updating HTML
-function formatCost(item, cost){
+function formatCost(item, cost) {
   // Use a function to format number so we can easily change it later (or based on user setting)
-  return item +	"&#32;" + "&#40;" + standard.format(cost,2,2) + "&#32;" + "Coins" + "&#41;";
+  return item + "&#32; &#40;" + standard.format(cost, 2, 2) + "&#32;Coins&#41;";
 }
-function setNumberValue(id, n){
+function setNumberValue(id, n) {
   // Set the innerHTML of the element with id=id to the formatted value
-  document.getElementById(id).innerHTML = standard.format(n,2,2);
+  document.getElementById(id).innerHTML = standard.format(n, 2, 2);
 }
-function setCostValue(id, item, cost){
+function setCostValue(id, item, cost) {
   document.getElementById(id).innerHTML = formatCost(item, cost);
 }
-function disable(id){
+function disable(id) {
   document.getElementById(id).disabled = true;
 }
-
-function updateView(){
-  setNumberValue("ore", game.ore);
-  setNumberValue("toughness", game.toughness);
-  setNumberValue("money", game.money);
-  setNumberValue("pickpower", game.pickPower);
-  setNumberValue("picklevel",game.pickLevel);
-  setNumberValue("overflow", game.vaultOverflow);
-  setNumberValue("vaultpower", game.vaultOverflow);
-  setNumberValue("minepower", game.sellPrice);
-  setNumberValue("ops", game.ops);
- setNumberValue("energy",game.energy);
-  setCostValue("pickaxebuy", "Upgrade	&#32;" + game.picks[game.area][game.nextPickNum][0], game.pickCost);
-  setCostValue("minebuy", game.mines[game.nextMineNum][0], game.mines[game.nextMineNum][1]);
-  setCostValue("vaultbuy", game.vaults[game.nextVaultNum][0], game.vaults[game.nextVaultNum][1]);
-  setCostValue("minerbuy", "Miner", game.minerCost);
-  setNumberValue("miners", game.miners);
-  if (game.nextPickNum === 6){
-    disable("pickaxebuy");
-  }
-  if (game.nextMineNum === 4){
-    disable("minebuy");
-  }
-   if (game.nextVaultNum === 5){
-    disable("vaultbuy");
-  }
-  for (var i = 0; i < game.upgrades.length; i++) {
-    if(game.money >= game.upgrades[i].criteria && game.upgrades[i].created == false){
-      createUpgrade(i);
-      game.upgrades[i].created = true;
-    }
-   document.getElementById("vault").value = game.ore;
-   document.getElementById("vault").max = game.vaultOverflow;
-  }
-}
-//Mining Code//
-function mine() {
-  game.ore += game.pickPower/game.toughness;
-  updateView();
-  overflow();
+function enable(id) {
+  document.getElementById(id).disabled = false;
 }
 //Vault Stuff//
-function overflow(){
+function overflow() {
   if (game.vaultOverflow < game.ore) {
     game.ore = game.vaultOverflow;
     updateView();
   }
 }
+
+//Mining Code//
+function mine() {
+  game.ore += game.pick.power / game.toughness;
+  updateView();
+  overflow();
+}
+
 //Selling Your Ore (since 2019)//
-function sell(){
+function sell() {
   game.money += game.sellPrice * game.ore;
   game.ore = 0;
   updateView();
 }
-//Buying new Pickaxes
-function buyNextPick(){
-  if (game.money >= game.pickCost){
-    game.pickPower *= 1+(0.1 * game.pickMultiply);
-    game.nextPickNum = Math.trunc(game.pickLevel/25);
-    game.money -= game.pickCost;
-    game.pickCost *= 1.125;
-    game.pickLevel++;
-    //if (nextPickNum == 6){nextPickNum...}
-    updateView();
+// Buying stuff.
+function buyNext(type) {
+  switch (type) {
+    case "pick":
+      if (game.money >= game.pick.cost) {
+        game.pick.power *= 1 + 0.1 * game.pick.multiply;
+        game.pick.next = Math.trunc(game.pick.level / 25);
+        game.money -= game.pick.cost;
+        game.pick.cost *= 1.125;
+        game.pick.level++;
+        //if (nextPickNum == 6){nextPickNum...}
+        updateView();
+      }
+      break;
+    case "mine":
+      if (game.money >= game.mines[game.area][game.nextMineNum][1]) {
+        game.sellPrice =
+          game.mines[game.area][game.nextMineNum][2] * game.mineMultiply;
+        game.toughness =
+          game.mines[game.area][game.nextMineNum][3] * game.toughnessMultiply;
+        game.mineCost = game.mines[game.area][game.nextMineNum][1];
+        game.money -= game.mineCost;
+        game.ops = game.miners * game.minerPower;
+        game.ops /= game.toughness * game.toughnessMultiply;
+        game.nextMineNum++;
+        updateView();
+      }
+      break;
+    case "vault":
+      if (game.money >= game.vaults[game.nextVaultNum][1]) {
+        game.vaultOverflow = game.vaults[game.nextVaultNum][2];
+        game.vaultCost = game.vaults[game.nextVaultNum][1];
+        game.money -= game.vaultCost;
+        game.nextVaultNum++;
+        updateView();
+      }
+      break;
   }
 }
-//Buying new Mines
-function buyNextMine(){
-  if (game.money >= game.mines[game.area][game.nextMineNum][1]){
-    game.sellPrice = game.mines[game.area][game.nextMineNum][2]*game.mineMupltiply;
-    game.toughness = game.mines[game.area][game.nextMineNum][3]*game.toughnessMultiply;
-    game.mineCost = game.mines[game.area][game.nextMineNum][1];
-    game.money -= game.mineCost;
-    game.ops = game.miners * game.minerPower;
-    game.ops /= game.toughness*game.toughnessMultiply;
-    game.nextMineNum++;
-    updateView();
-
-  }
-}
-//Buying new Vaults
-function buyNextVault(){
-  if (game.money >= game.vaults[game.nextVaultNum][1]){
-    game.vaultOverflow = game.vaults[game.nextVaultNum][2];
-    game.vaultCost = game.vaults[game.nextVaultNum][1];
-    game.money -= game.vaultCost;
-    game.nextVaultNum++;
-    updateView();
-  }
-}
-
-function buyMiner(){
-  if (game.money >=game.minerCost){
+function buyMiner() {
+  if (game.money >= game.minerCost) {
     game.miners++;
-   game.ops = game.miners * game.minerPower;
+    game.ops = game.miners * game.minerPower;
     game.ops /= game.toughness;
     game.money -= game.minerCost;
     game.minerCost *= 1.5;
     updateView();
   }
 }
-var last = Date.now();
-var goal = last + 1000;
 
-function handleInterval () {
-  last = Date.now();
-  if (last >= goal) {
-    goal = goal + 1000;
-    game.ore += game.ops;
-    updateView();
-    overflow();
-  }
+function handleInterval() {
+  game.ore += game.ops;
+  updateView();
+  overflow();
 }
-setInterval(handleInterval, 1000);
+let timer = setInterval(handleInterval, game.ticks);
 
+function genEnergy() {
+  game.energy++;
+  updateView();
+}
 
 // UPGRADE SYSTEM
 function buyUpgrade(id) {
-  if(game.money >= game.upgrades[id].cost){
+  if (game.money >= game.upgrades[id].cost) {
     game.money -= game.upgrades[id].cost;
-    switch(game.upgrades[id].buffType) {
-      case 'pickaxe':
-        game.pickMultiply *= game.upgrades[id].buffAmount;
-        game.pickPower *= game.pickMultiply;
-      break;
-     case 'mine':
+    switch (game.upgrades[id].buffType) {
+      case "pickaxe":
+        game.pick.multiply *= game.upgrades[id].buffAmount;
+        game.pick.power *= game.pick.multiply;
+        break;
+      case "mine":
         game.mineMultiply *= game.upgrades[id].buffAmount;
-        game.sellPrice *= game.mineMultiply
-      break;
-     case 'toughness':
+        game.sellPrice *= game.mineMultiply;
+        break;
+      case "toughness":
         game.toughnessMultiply *= game.upgrades[id].buffAmount;
-        game.toughness *= game.toughnessMultiply
-      break;
-      case 'miners':
-         game.minerPower *= game.upgrades[id].buffAmount;
-         game.ops *= game.minerPower
-       break;
-   }
-  let buttonElement = document.getElementsByClassName("upgradeButton")[0];
-  buttonElement.parentNode.removeChild(buttonElement);
- }
+        game.toughness *= game.toughnessMultiply;
+        break;
+      case "miners":
+        game.minerPower *= game.upgrades[id].buffAmount;
+        game.ops *= game.minerPower;
+        break;
+    }
+    let buttonElement = document.getElementsByClassName("upgradeButton")[0];
+    buttonElement.parentNode.removeChild(buttonElement);
+  }
 }
 
 function createUpgrade(id) {
   var newUpgrade = document.createElement("button");
-  var buttonContent = document.createTextNode(game.upgrades[id].name + "\n" + game.upgrades[id].discription + "\n" + game.upgrades[id].cost + " Coins");
+  var buttonContent = document.createTextNode(
+    game.upgrades[id].name +
+      "\n" +
+      game.upgrades[id].discription +
+      "\n" +
+      game.upgrades[id].cost +
+      " Coins"
+  );
   newUpgrade.className = "upgradeButton";
-  newUpgrade.onclick = function() {
+  newUpgrade.onclick = function () {
     buyUpgrade(id);
     return false;
-  }
+  };
   newUpgrade.appendChild(buttonContent);
-  document.getElementById("upgrademenu") .appendChild(newUpgrade);
+  document.getElementById("upgrademenu").appendChild(newUpgrade);
 }
-//Generate Energy
-function genEnergy(){
- if(game.ore >= 1){
-  game.ore -= 1;
-  game.energy+=1;
+try {
   updateView();
- }
- function save(){
-   window.localStorage.clear();
- var save = JSON.stringify(game);
- window.localStorage.setItem("game", save);
- console.log("Game Saved!");
- $.notify("Game Saved", "success");
- updateView();
- }
- setInterval(save,30000);
- function load(){
-   game=JSON.parse(localStorage.getItem('game'));
-   updateView();
- }
- if(localStorage.getItem('game') !== null){
- load();
- $.notify("Game Loaded", "info");
- }else{
+}
+catch(err) {
+  console.error(err)
+  
+}
+finally{
+  window.localStorage.clear();
+}
+function save() {
+  window.localStorage.clear();
+  let save = JSON.stringify(game);
+  window.localStorage.setItem("game", save);
+  console.log("Game Saved!");
+  $.notify("Game Saved", "success");
+  updateView();
+}
+setInterval(save, 30000);
+
+function load() {
+  game = JSON.parse(localStorage.getItem("game"));
+  updateView();
+}
+if (localStorage.getItem("game") !== null) {
+  load();
+  $.notify("Game Loaded", "info");
+} else {
   $.notify("No Save Game Found", "warn");
- }
+}
+
+function updateView() {
+  setNumberValue("ore", game.ore);
+  setNumberValue("toughness", game.toughness);
+  setNumberValue("money", game.money);
+  setNumberValue("pickpower", game.pick.power);
+  setNumberValue("picklevel", game.pick.level);
+  setNumberValue("overflow", game.vaultOverflow);
+  setNumberValue("vaultpower", game.vaultOverflow);
+  setNumberValue("minepower", game.sellPrice);
+  setNumberValue("ops", game.ops);
+  setNumberValue("energy", game.energy);
+  setCostValue("minerbuy", "Miner", game.minerCost);
+  setNumberValue("miners", game.miners);
+  if (game.pick.next === 6) {
+    disable("pickaxebuy");
+  } else {
+    setCostValue(
+      "pickaxebuy",
+      "Upgrade	&#32;" + game.pick.names[game.area][game.pick.next][0],
+      game.pick.cost
+    );
+
+  }
+  if (game.nextMineNum === 4) {
+    disable("minebuy");
+  } else{
+    setCostValue(
+      "minebuy",
+      game.mines[game.area][game.nextMineNum][0],
+      game.mines[game.area][game.nextMineNum][1]
+    );
+
+  }
+  if (game.nextVaultNum === 5) {
+    disable("vaultbuy");
+    setCostValue(
+      "vaultbuy",
+      game.vaults[game.nextVaultNum][0],
+      game.vaults[game.nextVaultNum][1]
+    );
+  }
+  for (var i = 0; i < game.upgrades.length; i++) {
+    if (
+      game.money >= game.upgrades[i].criteria &&
+      game.upgrades[i].created == false
+    ) {
+      createUpgrade(i);
+      game.upgrades[i].created = true;
+    }
+    document.getElementById("vault").value = game.ore;
+    document.getElementById("vault").max = game.vaultOverflow;
+  }
+}
+
